b13ae98803f8edb9e5d1512efc0210365bc3fa87
Supasate Choochaisri <supasate.c@gmail.com>
2016-05-26 16:18:51 +0700

    Generate district mapping utility

---
+++ qualification/season_1/src/tools/district_mapping_generator.py
@@ -0,0 +1,18 @@
+cluster_map_path = '../../training_data/cluster_map/cluster_map'
+dict_content = ""
+with open(cluster_map_path, 'r') as f:
+    for line in f.readlines():
+        district_hash, district_id = line.strip().split()
+        formatted_line = '    "' + district_hash + '" : ' + district_id + ',\n'
+        dict_content += formatted_line
+dict_content = dict_content[:-2]
+
+output = 'district_map = {\n' + dict_content + '\n}\n'
+output += """
+def get_district_id(hash):
+    return district_map[hash]
+"""
+
+output_path = './district_mapping.py'
+with open(output_path, 'w') as f:
+    f.write(output)

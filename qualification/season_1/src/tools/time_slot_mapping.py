099d49d0e56f3c5abc6855d71eb1da74f21aa9cd
Supasate Choochaisri <supasate.c@gmail.com>
2016-05-26 17:03:33 +0700

    Rename function and inject slot duration

--- qualification/season_1/src/tools/time_slot_mapping.py
+++ qualification/season_1/src/tools/time_slot_mapping.py
@@ -1,5 +1,5 @@
-def get_time_slot(datetime):
-    date, time = datetime.split()
+def get_datetime_slot(datetime, slot_duration_in_minute):
+    date, time = datetime.strip().split()
     hh, mm, ss = map(int, time.split(':'))
     cum_minutes = (hh * 60) + mm + 1
-    return date + '-' + str(cum_minutes / 10)
+    return date + '-' + str((cum_minutes / slot_duration_in_minute) + 1)

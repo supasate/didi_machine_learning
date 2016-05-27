ce91f8f7b3314210f8cb55887893f17bd89e7d3a
Supasate Choochaisri <supasate.c@gmail.com>
2016-05-27 13:54:57 +0700

    Fix time slot bug

--- qualification/season_1/src/tools/time_slot_mapping.py
+++ qualification/season_1/src/tools/time_slot_mapping.py
@@ -1,5 +1,5 @@
 def get_datetime_slot(datetime, slot_duration_in_minute):
     date, time = datetime.strip().split()
     hh, mm, ss = map(int, time.split(':'))
-    cum_minutes = (hh * 60) + mm + 1
+    cum_minutes = (hh * 60) + mm 
     return date + '-' + str((cum_minutes / slot_duration_in_minute) + 1)

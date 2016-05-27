5f1fae1ce4db480325ecb6e890f2656c5434f1ab
Supasate Choochaisri <supasate.c@gmail.com>
2016-05-27 17:27:19 +0700

    Handle 1D list to calculate MAPE score

--- qualification/season_1/src/tools/mape.py
+++ qualification/season_1/src/tools/mape.py
@@ -1,9 +1,18 @@
 def mape_score(label_gaps, predicted_gaps):
-    num_district = len(label_gaps)
-    num_timeslot = len(label_gaps[0])
+    if len(label_gaps) < 1:
+        return None
+
     score = 0
-    for d in range(num_district):
-        for t in range(num_timeslot):
-            score += abs((label_gaps[d][t] - predicted_gaps[d][t]) / float(label_gaps[d][t]))
-    score = score / (num_district * num_timeslot)
+    if label_gaps[0] is list:
+        num_district = len(label_gaps)
+        num_timeslot = len(label_gaps[0])
+        score = 0
+        for d in range(num_district):
+            for t in range(num_timeslot):
+                score += abs((label_gaps[d][t] - predicted_gaps[d][t]) / float(label_gaps[d][t]))
+        score = score / (num_district * num_timeslot)
+    else:
+        for i in range(len(label_gaps)):
+            score += abs((label_gaps[i] - predicted_gaps[i]) / float(label_gaps[i]))
+        score = score / len(label_gaps)
     return score

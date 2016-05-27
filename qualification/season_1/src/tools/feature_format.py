eff9c2057b31cd181ab96bec2efbedaed30aaef7
Supasate Choochaisri <supasate.c@gmail.com>
2016-05-27 17:37:47 +0700

    Able to do regression

--- qualification/season_1/src/tools/feature_format.py
+++ qualification/season_1/src/tools/feature_format.py
@@ -1,7 +1,17 @@
-def targetFeatureSplit(data):
+def load_data(file_path):
+    data = []
+    with open(file_path, 'r') as f:
+        for line in f.readlines():
+            if line[0] == '#':
+                continue
+            district_id, timeslot, day_of_week, weather, celsius, pm25, tf_lv1, tf_lv2, tf_lv3, tf_lv4, gap_t1, gap_t2, gap = line.strip().split()
+            data.append([int(district_id), int(timeslot), int(day_of_week), int(weather), float(celsius), float(pm25), int(tf_lv1), int(tf_lv2), int(tf_lv3), int(tf_lv4), int(gap_t1), int(gap_t2), int(gap)])
+    return data
+
+def target_feature_split(data):
     target = []
     features = []
-    for item in date:
+    for item in data:
         target.append(item[len(item) - 1])
         features.append(item[: -1])
 

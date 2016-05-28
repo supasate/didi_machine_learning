e213878c317df352f5bc50c8ce33d0bba191ce0f
Supasate Choochaisri <supasate.c@gmail.com>
2016-05-28 14:36:47 +0700

    Add demand and supply data in gap files

--- qualification/season_1/src/prepare/create_gaps_data.py
+++ qualification/season_1/src/prepare/create_gaps_data.py
@@ -1,41 +1,71 @@
 import os
+from time import time
 from collections import defaultdict
 from day_of_week import get_day_of_week
 
-processed_data_folder = '../../processed_data'
+# Count supply, demand, gap from an order data file
+def count_demand_supply_gap(input_path):
+    # initialize counter of supply, demand, gap
+    counter = {'gap': {}, 'demand': {}, 'supply': {}}
+    for district_id in range(1, 67):
+        counter['gap'][str(district_id)] = defaultdict(int)
+        counter['demand'][str(district_id)] = defaultdict(int)
+        counter['supply'][str(district_id)] = defaultdict(int)
 
-for dataset in ['training_data', 'test_set_1']:
-    order_folder = '../../cleansed_data/' + dataset + '/order_data'
-    gaps_folder = processed_data_folder + '/gaps_data/' + dataset
+    # Counting supply, demand, and gap
+    with open(input_path, 'r') as f:
+        for line in f.readlines():
+            # Split each field from the file
+            order_id, driver_id, passenger_id, start_district_id, dest_district_id, price, datetime_slot = line.strip().split()
+            counter['demand'][start_district_id][datetime_slot] += 1
 
-    if not os.path.exists(gaps_folder):
-        os.makedirs(gaps_folder)
+            if driver_id == 'NULL':
+                counter['gap'][start_district_id][datetime_slot] += 1
+            else:
+                counter['supply'][start_district_id][datetime_slot] += 1
+    return counter
 
-    for file_name in os.listdir(order_folder):
-        gaps_count = dict()
-        for district_id in range(1, 67):
-            gaps_count[str(district_id)] = defaultdict(int)
+# Save counting into a file sorted by district_id and datetime_slot
+def save_couting(output_path, counter_dict):
+    with open(output_path, 'w') as f:
+        f.write("# district_id\tdatetime_slot\tday_of_week\tdemand\tsupply\tgap\n")
 
-        input_file = order_folder + '/' + file_name
-        with open(input_file, 'r') as f:
-            for line in f.readlines():
-                order_id, driver_id, passenger_id, start_district_id, dest_district_id, price, timeslot = line.strip().split()
-                if timeslot not in gaps_count[start_district_id]:
-                    gaps_count[start_district_id][timeslot] = 0
+        # Sort gap count by district_id first
+        sorted_gaps_count = sorted(counter_dict['gap'].items(), key = lambda x: x[0])
+        for gap in sorted_gaps_count:
+            district_id = gap[0]
+            datetime_slot_gaps_count = gap[1]
+            # Then, sort by slot (part of datetime_slot)
+            sorted_datetime_slot_gaps_count = sorted(datetime_slot_gaps_count.items(), key = lambda x: int(x[0].split('-')[3]))
+            for datetime_slot_gap in sorted_datetime_slot_gaps_count:
+                datetime_slot = datetime_slot_gap[0]
+                day_of_week = get_day_of_week(datetime_slot)
+                gap_count = datetime_slot_gap[1]
+                demand_count = counter_dict['demand'][district_id][datetime_slot]
+                supply_count = counter_dict['supply'][district_id][datetime_slot]
+                f.write("%s\t%s\t%s\t%s\t%s\t%s\n" % (district_id, datetime_slot, day_of_week, demand_count, supply_count, gap_count))
 
-                if driver_id == 'NULL':
-                    gaps_count[start_district_id][timeslot] += 1
+if __name__ == "__main__":
+    t0 = time()
 
-        sorted_gaps_count = sorted(gaps_count.items(), key = lambda x: x[0])
-        output_file = gaps_folder + '/gaps_' + file_name
-        with open(output_file, 'w') as f:
-            f.write("#district_id\tdatetime_slot\tday_of_week\tgaps\n")
-            for gap in sorted_gaps_count:
-                district_id = gap[0]
-                timeslot_gaps_count = gap[1]
-                sorted_timeslot_gaps_count = sorted(timeslot_gaps_count.items(), key = lambda x: int(x[0].split('-')[3]))
-                for timeslot_gap in sorted_timeslot_gaps_count:
-                    timeslot = timeslot_gap[0]
-                    day_of_week = get_day_of_week(timeslot)
-                    count = timeslot_gap[1]
-                    f.write("%s\t%s\t%s\t%s\n" % (district_id, timeslot, day_of_week, count))
+    OUTPUT_FOLDER_PREFIX = '../../processed_data/gaps_data/'
+
+    for dataset in ['training_data', 'test_set_1']:
+        OUTPUT_FOLDER = OUTPUT_FOLDER_PREFIX + dataset
+
+        # Create folder if not exist
+        if not os.path.exists(OUTPUT_FOLDER):
+            os.makedirs(OUTPUT_FOLDER)
+
+        # Each file represents orders in each da
+        ORDER_DATA_FOLDER = '../../cleansed_data/' + dataset + '/order_data'
+
+        for file_name in os.listdir(ORDER_DATA_FOLDER):
+            # Get counter of supply, demand, gap
+            input_path = ORDER_DATA_FOLDER + '/' + file_name
+            counter = count_demand_supply_gap(input_path)
+
+            # Save couting into a file
+            output_path = OUTPUT_FOLDER + '/gaps_' + file_name
+            save_couting(output_path, counter)
+    print("Creating gaps data time:", round(time() - t0, 3), "s")

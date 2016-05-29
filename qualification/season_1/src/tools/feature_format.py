8794c790b3f027a14ffc02a171c8e98007d3fba4
Supasate Choochaisri <supasate.c@gmail.com>
2016-05-29 12:24:17 +0700

    Refactor load and split training data

--- qualification/season_1/src/tools/feature_format.py
+++ qualification/season_1/src/tools/feature_format.py
@@ -1,18 +1,14 @@
-def load_data(file_path):
-    data = []
+# Load and split into metadata, features, target lists
+def load_and_split_training_data(file_path):
+    metadata_list = []
+    features_list = []
+    target_list = []
     with open(file_path, 'r') as f:
         for line in f.readlines():
             if line[0] == '#':
                 continue
-            district_id, timeslot, day_of_week, weather, celsius, pm25, tf_lv1, tf_lv2, tf_lv3, tf_lv4, gap_t1, gap_t2, gap = line.strip().split()
-            data.append([int(district_id), int(timeslot), int(day_of_week), int(weather), float(celsius), float(pm25), int(tf_lv1), int(tf_lv2), int(tf_lv3), int(tf_lv4), int(gap_t1), int(gap_t2), int(gap)])
-    return data
-
-def target_feature_split(data):
-    target = []
-    features = []
-    for item in data:
-        target.append(item[len(item) - 1])
-        features.append(item[: -1])
-
-    return target, features
+            district_id, date, timeslot, day_of_week, weather_t1, celsius_t1, pm25_t1, weather_t2, celsius_t2, pm25_t2, tf_lv1_t1, tf_lv2_t1, tf_lv3_t1, tf_lv4_t1, tf_lv1_t2, tf_lv2_t2, tf_lv3_t2, tf_lv4_t2, demand_t1, supply_t1, gap_t1, demand_t2, supply_t2, gap_t2, gap = line.strip().split()
+            metadata_list.append([district_id, date + '-' + timeslot])
+            features_list.append([int(district_id), int(timeslot), int(day_of_week), int(weather_t1), float(celsius_t1), float(pm25_t1), int(weather_t2), float(celsius_t2), float(pm25_t2), int(tf_lv1_t1), int(tf_lv2_t1), int(tf_lv3_t1), int(tf_lv4_t1), int(tf_lv1_t2), int(tf_lv2_t2), int(tf_lv3_t2), int(tf_lv4_t2), int(demand_t1), int(supply_t1), int(gap_t1), int(demand_t2), int(supply_t2), int(gap_t2)])
+            target_list.append(int(gap))
+    return metadata_list, features_list, target_list

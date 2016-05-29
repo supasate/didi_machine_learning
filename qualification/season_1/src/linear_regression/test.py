145022639adfba716b93a2eeb231b1cd261b2a02
Supasate Choochaisri <supasate.c@gmail.com>
2016-05-29 21:58:04 +0700

    Make model name to be loaded easier

--- qualification/season_1/src/linear_regression/test.py
+++ qualification/season_1/src/linear_regression/test.py
@@ -5,16 +5,19 @@ from feature_format import load_and_split_data
 from model_helper import load_latest_model
 from result_helper import save_result
 
+# Model name to be loaded
+MODEL_NAME = 'linear_regression'
+
 if __name__ == "__main__":
     # Load and split data into metadata, features, target
     data_file = '../../processed_data/prediction_data/to_predict_features'
     metadata_list, features_list, target_list = load_and_split_data(data_file)
 
     # Load saved model
-    model = load_latest_model('./model', 'linear_regression')
+    model = load_latest_model('./model', MODEL_NAME)
 
     predictions = model.predict(features_list)
 
     OUTPUT_FOLDER = './output'
 
-    save_result(OUTPUT_FOLDER, 'linear_regression', metadata_list, predictions)
+    save_result(OUTPUT_FOLDER, MODEL_NAME, metadata_list, predictions)
